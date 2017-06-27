8e867f3da34d4b33707258bd342ea488333e0130
findneo <1285470533@qq.com>
2017-06-27 17:41:13 +0800

    6-27

--- quitSys.bat
+++
@@ -1,8 +0,0 @@
-taskkill /F /IM QQ.exe 
-taskkill /F /IM Shadowsocks.exe
-taskkill /F /IM Everything.exe 
-taskkill /F /IM huyan.exe 
-taskkill /F /IM GoldenDict.exe
-taskkill /F /IM chrome.exe
-taskkill /F /IM Evernote.exe
-shutdown -i -s 
--- wooyun_genMenuOfKnowLib.py
+++ wooyun_genMenuOfKnowLib.py
@@ -1,23 +1,21 @@
-# 为乌云漏洞库生成目录
+# 为乌云知识库生成目录
 import os
 f0 = open('index.html', 'w')
-# f.write('Hello, world!')
 cnt = 1
-list = os.listdir('bugs')
+list = os.listdir('drops')
 for i in range(0, len(list)):
-    path = os.path.join('bugs', list[i])
+    path = os.path.join('drops', list[i])
     if os.path.isfile(path):
         f = open(path, 'r')
         t = 0
         for li in f.readlines():
             t = t + 1
-            if t == 6:
+            if t == 5:
                 f0.write('<li><a href="' + path + '">' +
-                         '\t' + li[7:-22] + '</a></li>' + '\n')
+                         '\t' + li[9:-27] + '</a></li>' + '\n')
                 break
         cnt = cnt + 1
         f.close()
+
 f0.close()
 print cnt
-# 40294 means success
-# this script cost about 1min or so
